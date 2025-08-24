cab2463d6ca5b414e00e56ef8d87932d87ab39a1
AR <ar.bjm75@gmail.com>
2025-08-24 09:28:08 +0800

    Add files via upload

--- lapse.mjs
+++ lapse.mjs
@@ -1867,6 +1867,6 @@ setTimeout(PayloadLoader("aio_patches.bin"),500);
 log("AIO Fixes Applied.!");
 //Load GoldHEN :)
 setTimeout(PayloadLoader("goldhen_2.4b18.5.bin"),500);
-msgs.innerHTML = "AR Playstation";
+msgs.innerHTML = "Ty For AR Playstation";
 
 })
